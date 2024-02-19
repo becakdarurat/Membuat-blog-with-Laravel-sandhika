9dcbe8d8000b0fe1add689e3bea487e37f7dc6f8
SofyanTanjung <sofyant757@gmail.com>
2024-02-19 08:15:08 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wpu Blog | About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="{{ $img }}" alt="" width="200">
+</body>
+</html>
